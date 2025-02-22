b78ba31d243a1ad985246c334b1ee6f5c765b296
Jingyue Wu <jingyue804@gmail.com>
2025-02-21 23:15:16 -0500

    Device and channel renaming and disconnecting

--- ozys-app/src/components/devices/ChannelCard.tsx
+++ ozys-app/src/components/devices/ChannelCard.tsx
@@ -1,13 +1,86 @@
 import menuIcon from '../../assets/menu.png'
+import { useState } from 'react'
+import { OzysDevice } from '../../device/OzysDevice'
 
-export default function ChannelCard({ ...props }) {
+interface ISensorData {
+  connected: true
+  enabled: boolean
+  name: string
+  id: string
+}
+
+export default function ChannelCard(props: {
+  device: OzysDevice
+  sensorData: ISensorData
+}) {
   const name = props.sensorData.name
+  const channelId = props.sensorData.id
+
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [renameInput, setRenameInput] = useState(false)
+  const [renameValue, setRenameValue] = useState(name)
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
+  const renameChannel = async () => {
+    props.device.renameChannel(channelId, renameValue)
+    setRenameInput(false)
+    setMenuOpen(false)
+  }
+
+  const closePopup = () => {
+    if (menuOpen) {
+      setMenuOpen(false)
+    }
+  }
+
   return (
-    <div className='w-full h-24 bg-[#F5F5F5] rounded-lg p-2'>
+    <div
+      className='w-full h-24 bg-[#F5F5F5] rounded-lg p-2'
+      onClick={closePopup}
+    >
       <div className='flex justify-between'>
-        <h1 className='text-md'>{name}</h1>
+        {renameInput ? (
+          <form className='w-12 flex gap-4'>
+            <input
+              type='text'
+              placeholder='Enter new name'
+              className='px-2 py-1 w-36 bg-white focus:outline-gray-300 rounded-lg'
+              defaultValue={name}
+              onChange={(e) => setRenameValue(e.target.value)}
+            />
+            <button
+              className='bg-white px-6 rounded-lg hover:bg-gray-100'
+              type='submit'
+              onClick={renameChannel}
+            >
+              Submit
+            </button>
+          </form>
+        ) : (
+          <h1 className='text-lg'>{name}</h1>
+        )}
         <div className='h-6 w-6'>
-          <img src={menuIcon} alt='' />
+          <button onClick={toggleMenu}>
+            <img src={menuIcon} alt='' />
+          </button>
+
+          {menuOpen ? (
+            <div
+              className={`flex flex-col items-start bg-[#F7F7F7] z-50 w-[150px] drop-shadow-lg divide-y divide-gray-300 -translate-x-36`}
+            >
+              <button
+                className='px-2 py-1 w-full text-center hover:bg-[#E2E2E2]'
+                onClick={() => setRenameInput(true)}
+              >
+                Rename Device
+              </button>
+            </div>
+          ) : (
+            ''
+          )}
         </div>
       </div>
     </div>
--- ozys-app/src/components/devices/DeviceCard.tsx
+++ ozys-app/src/components/devices/DeviceCard.tsx
@@ -2,7 +2,7 @@ import { observer } from 'mobx-react-lite'
 import { OzysDevice } from '../../device/OzysDevice'
 import ChannelCard from './ChannelCard'
 import menuIcon from '../../assets/menu.png'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useOzysDevicesManager } from '../../device/OzysDevicesManager'
 
 export const DeviceCard = observer((props: { device: OzysDevice }) => {
@@ -11,7 +11,7 @@ export const DeviceCard = observer((props: { device: OzysDevice }) => {
 
   const [menuOpen, setMenuOpen] = useState(false)
   const [renameInput, setRenameInput] = useState(false)
-  const [renameValue, setRenameValue] = useState('')
+  const [renameValue, setRenameValue] = useState(deviceInfo.name)
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen)
@@ -24,10 +24,49 @@ export const DeviceCard = observer((props: { device: OzysDevice }) => {
     setMenuOpen(false)
   }
 
+  const closePopup = () => {
+    if (menuOpen) {
+      setMenuOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      closePopup()
+    })
+  })
+
   return (
-    <div className='flex flex-col w-full pb-8 gap-4 p-4'>
-      <div className='flex justify-between mx-2'>
-        <h1 className='text-lg font-semibold'>{deviceInfo.name}</h1>
+    <div className='flex flex-col w-full pb-8 gap-4 p-4' onClick={closePopup}>
+      <div className='flex justify-between mr-2'>
+        {renameInput ? (
+          <form
+            className='w-72 flex gap-4 mr-4'
+            onSubmit={(e) => {
+              e.preventDefault() // Prevent default form submission
+              renameDevice()
+            }}
+          >
+            <input
+              type='text'
+              placeholder='Enter new name'
+              className='px-2 py-1 w-36 bg-gray-100 focus:outline-gray-400 rounded-lg'
+              defaultValue={deviceInfo.name}
+              onChange={(e) => {
+                setRenameValue(e.target.value)
+              }}
+            />
+            <button
+              className='bg-gray-100 px-6 rounded-lg hover:bg-gray-200'
+              type='submit'
+            >
+              Submit
+            </button>
+          </form>
+        ) : (
+          <h1 className='text-lg font-semibold'>{deviceInfo.name}</h1>
+        )}
+
         <div className='h-6 w-6'>
           <button onClick={toggleMenu}>
             <img src={menuIcon} alt='' />
@@ -53,31 +92,17 @@ export const DeviceCard = observer((props: { device: OzysDevice }) => {
           ) : (
             ''
           )}
-
-          {renameInput ? (
-            <div className='z-50 bg-[#F7F7F7] p-2 w-[150px] -translate-x-36 flex flex-col gap-6 drop-shadow-lg justify-center'>
-              <form className='w-12'>
-                <input
-                  type='text'
-                  placeholder='Enter new name'
-                  className='px-2 py-1 w-32'
-                  onChange={(e) => setRenameValue(e.target.value)}
-                />
-                <button type='submit' onClick={renameDevice}>
-                  Submit
-                </button>
-              </form>
-            </div>
-          ) : (
-            ''
-          )}
         </div>
       </div>
 
       {deviceInfo.channels
         .filter((channel) => channel.connected)
         .map((channel) => (
-          <ChannelCard key={channel.id} sensorData={channel} />
+          <ChannelCard
+            key={channel.id}
+            sensorData={channel}
+            device={props.device}
+          />
         ))}
     </div>
   )
