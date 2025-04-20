0198599cd850feafdae88bad4eb6ab5443283545
Pegasis <me@pegasis.site>
2025-04-20 10:47:52 -0400

    remove can bus crate

--- can-bus/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![cfg_attr(not(test), no_std)]
-
-pub mod message;
-pub mod messages;
-pub mod id;
-pub mod node_types;
--- can-bus/src/node_types.rs
+++
@@ -1,5 +0,0 @@
-// the higher the number, the higher the priority
-// the maximum node type is 63
-
-pub const VOID_LAKE_NODE_TYPE: u8 = 40;
-pub const STRAIN_GAUGES_NODE_TYPE: u8 = 20;
